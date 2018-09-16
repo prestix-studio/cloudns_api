b1950516a0319e97df847b7325504baa8112f0a7
Harold Bradley III <hbradleyiii@bradleystudio.net>
2018-09-16 14:09:37 -0400

    Adds barebones module.

---
+++ cloudns_api/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# name:             cloudns_api/__init__.py
+# author:           Harold Bradley III
+# email:            harold@bradleystudio.net
+# created on:       09/15/2018
+#
+
+"""
+cloudns_api
+~~~~~~~~~~~
+
+This package contains interface functions to make calls to the ClouDNS.net API.
+"""
+
+from __future__ import absolute_import
+
+
+__title__ = 'cloudns_api'
+__version__ = '0.1'
+__author__ = 'Harold Bradley III'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018 Harold Bradley III'
+
+# Soli Deo gloria. <><
