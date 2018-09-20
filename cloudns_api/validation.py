3dfcc64bbef9b9d03d329c8f76bd5769908bbf7a
Harold Bradley III <hbradleyiii@bradleystudio.net>
2018-09-20 06:32:41 -0400

    Adds more validation functions.

--- cloudns_api/validation.py
+++ cloudns_api/validation.py
@@ -195,14 +195,19 @@ def is_api_bool(value, fieldname, **kwargs):
 # Set up validation functions dict
 validation_functions = {
     'admin-mail':    is_email,
+    'bool':          is_api_bool,
     'domain-name':   is_domain_name,
     'default-ttl':   is_int,
     'email':         is_email,
+    'frame':         is_api_bool,
     'integer':       is_int,
+    'port':          is_int,
     'primary-ns':    is_domain_name,
+    'priority':      is_int,
     'record':        is_required,
     'refresh':       is_int,
     'required':      is_required,
     'type':          is_record_type,
     'ttl':           is_ttl,
+    'weight':        is_int,
 }
