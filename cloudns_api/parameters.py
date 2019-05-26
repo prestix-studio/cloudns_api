bdc87424b2c2a34e390013a4f0befddbc24fd815
Harold Bradley III <hbradleyiii@bradleystudio.net>
2019-05-26 14:24:25 -0400

    Adds parameters class.

---
+++ cloudns_api/parameters.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+#
+# name:             parameters.py
+# author:           Harold Bradley III | Prestix Studio, LLC.
+# email:            harold@prestix.studio
+# created on:       05/26/2019
+#
+
+"""
+cloud_api.parameters
+~~~~~~~~~~~~~~~~~~~~
+
+This module contains the Parameters class for self-validating parameters
+objects.
+
+The object allows for declaring parameters with validation options for use in
+self-validating (or manual validation).
+"""
+
+from .api import get_auth_params
+from .validation import validate, ValidationError, ValidationErrorsBatch
+
+
+class Parameters(object):
+    def __init__(self, params_with_options, validate=True):
+        """Initializes the Parameters object
+
+        :param params_with_options: dict, a dict of parameters with their
+            options for validating.
+            Format without options:
+                {
+                    'fieldname' : value,
+                    ...
+                }
+            Format with options:
+                {
+                    'fieldname' : {
+                        'value': value,
+                        'optional': is_optional,
+                        ...
+                    },
+                    ...
+                }
+        :param validate: boolean, whether or not to validate parameters on
+            initialization. Defaults to True.
+        """
+        self._params_with_options = {**get_auth_params(), **params_with_options}
+
+        if (validate):
+            self.validate()
+
+    def to_dict(self):
+        """Returns a dict of just the fieldname and values."""
+        return {fieldname:options['value'] if 'value' in options else options
+                for fieldname, options in self._params_with_options.items()}
+
+    def validate(self):
+        """Validates the parameters according to the fieldname and any optional
+        validation options."""
+        errors = []
+
+        for fieldname, options in self._params_with_options.items():
+            value = options['value'] if 'value' in options else options
+            optional = options['optional'] if 'optional' in options else False
+
+            try:
+                validate(fieldname, value, options)
+            except ValidationError as e:
+                errors.append(e)
+
+        if len(errors) == 1:
+            raise errors[0]
+        elif len(errors) > 1:
+            raise ValidationErrorsBatch(errors)
